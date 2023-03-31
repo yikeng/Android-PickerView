4b35e129f16dc5e4e5f4a76324a1b2f1b34f8c39
Shanks <2573475062@qq.com>
2023-03-31 14:44:22 +0800

    Migrate to AndroidX

--- app/src/main/java/com/bigkoo/pickerviewdemo/FragmentTestActivity.java
+++ app/src/main/java/com/bigkoo/pickerviewdemo/FragmentTestActivity.java
@@ -2,10 +2,10 @@ package com.bigkoo.pickerviewdemo;
 
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FragmentTestActivity extends AppCompatActivity {
     private FragmentManager mFragmentManager;
